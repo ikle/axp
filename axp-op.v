1a284b7f57ccd50cfa4a09707b6613041edbc710
Alexei A. Smekalkine <ikle@ikle.ru>
2022-10-22 00:00:16 +0300

    axp-op: implement byte manipulation and shift unit (ISA compliant)

--- axp-op.v
+++ axp-op.v
@@ -63,4 +63,52 @@ module axp_logic #(
 	assign y = cond ? rm : cb;
 endmodule
 
+/* opcode 12, byte manipulation and shift */
+
+module axp_shift (
+	input [6:0] f, input [63:0] x, input [63:0] n, output [63:0] y
+);
+	wire [5:0] ns = f[1] ? n[8:3] : n[5:0];		/* scale	*/
+	wire [5:0] nn = f[0] ? -ns : ns;		/* negate	*/
+
+	wire signed [127:0] sx = x << 64;
+	wire signed [127:0] sy = f[3:2] == 3 ? sx >>> nn : sx >> nn;
+
+	assign y = f[2] ? sy[127:64] : f[3] ? sy[63:0] : x;
+endmodule
+
+module axp_mask (
+	input [6:0] f, input [63:0] x, output [63:0] y
+);
+	wire [15:0] m  = {{4 {f[5] & f[4]}}, {2 {f[5]}}, (f[5] | f[4]), 1'b1};
+	wire        q0 = (f[3:2] == 0);
+	wire [15:0] ms = (q0 | f[0]) ? m << x[2:0] : m;	/* msk or ins	*/
+	wire [15:0] mh = f[6] & ~f[3] ? ms >> 8 : ms;	/* high part	*/
+	wire [7:0]  mf = f[1] ? mh :			/* byte ops	*/
+			 q0   ?  x : 8'hff;		/* zap* / shift	*/
+//	assign y = (f[3:0] == 0 || f[3:1] == 1) ? ~mf : mf;
+	assign y = (q0 && !f[0]) ? ~mf : mf;		/* zap or msk?	*/
+endmodule
+
+module axp_zapnot (
+	input [63:0] x, input [63:0] mask, output [63:0] y
+);
+	genvar i;
+
+	for (i = 0; i < 64; i = i + 1)
+		assign y[i] = x[i] & mask[i >> 3];
+endmodule
+
+module axp_shifter (
+	input  [31:0] cmd, input [63:0] a, input [63:0] b,
+	output [63:0] y
+);
+	wire [6:0] f = cmd[11:5];
+	wire [63:0] m, s;
+
+	axp_shift  S (f, a, b, s);
+	axp_mask   M (f, b, m);
+	axp_zapnot Z (s, m, y);
+endmodule
+
 `endif  /* AXP_OP_V */
