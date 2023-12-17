9bad997a6bfef83d767611bd038c1697077c6892
Alexei A. Smekalkine <ikle@ikle.ru>
2023-12-17 04:36:29 +0300

    axp-op-10-test: support CMPBGE instruction

--- axp-op-10-test.c
+++ axp-op-10-test.c
@@ -23,7 +23,24 @@ static inline char mp_digit_adc (uint64_t *r, uint64_t x, uint64_t y, int c)
 	return c + mp_digit_add (r, x, a);
 }
 
-/* opcode 10, adder and comparator (w/o cmpbge) */
+static inline int axp_byte_carry (int a, int b, int ci)
+{
+	return ((a & 0xff) + (b & 0xff) + ci) >> 8;
+}
+
+static inline uint64_t axp_cmpbge (int f, uint64_t a, uint64_t bb, int ci)
+{
+	return	axp_byte_carry (a >>  0, bb >>  0, ci) << 0 |
+		axp_byte_carry (a >>  8, bb >>  8,  1) << 1 |
+		axp_byte_carry (a >> 16, bb >> 16,  1) << 2 |
+		axp_byte_carry (a >> 24, bb >> 24,  1) << 3 |
+		axp_byte_carry (a >> 32, bb >> 32,  1) << 4 |
+		axp_byte_carry (a >> 40, bb >> 40,  1) << 5 |
+		axp_byte_carry (a >> 48, bb >> 48,  1) << 6 |
+		axp_byte_carry (a >> 56, bb >> 56,  1) << 7;
+}
+
+/* opcode 10, adder and comparator (with cmpbge) */
 
 static inline uint64_t axp_adder (int f, uint64_t a, uint64_t b)
 {
@@ -40,7 +57,9 @@ static inline uint64_t axp_adder (int f, uint64_t a, uint64_t b)
 	int eq  = (f & 0x20) && (sum == 0);		/* 20 - eq	*/
 	int ult = (f & 0x10) && (!co);			/* 10 - ult	*/
 
-	return (f & 0x04) ? (ult | eq | lt) : ss;	/* 04 - cmp	*/
+	int cmp = (f & 0x02) ? axp_cmpbge (f, a, bb, cin) : (ult | eq | lt);
+
+	return (f & 0x04) ? cmp : ss;			/* 04 - cmp	*/
 }
 
 int main (int argc, char *argv[])
