837f1e1987270db285d395e560c58c2cb97d68b2
Alexei A. Smekalkine <ikle@ikle.ru>
2024-01-15 01:11:30 +0300

    axp-op: restyle axp_branch, step 2

--- axp-op.v
+++ axp-op.v
@@ -137,8 +137,8 @@ module axp_branch (
 );
 	wire [5:0] op = cmd[31:26];
 
-	wire lbc  = op[3] ? ~a[0] : ~0;			/* 08 - lbc/abs	*/
-	wire eq   = op[0] ? ~|a : lbc;			/* 01 - eq/lbc	*/
+	wire lbs  = op[3] ? a[0] : 0;			/* 08 - lbs/abs	*/
+	wire eq   = op[0] ? ~|a : ~lbs;			/* 01 - eq/lbc	*/
 	wire le   = op[1] & a[63] | eq;			/* 02 - lt	*/
 	wire cond = op[2] ? ~le : le;			/* 04 - invert	*/
 
