a800349be55787136e26a759dd35976d47135fe6
Alexei A. Smekalkine <ikle@ikle.ru>
2024-01-15 06:44:15 +0300

    axp-op-11-test: move condition calculation up, no functional changes

--- axp-op-11-test.c
+++ axp-op-11-test.c
@@ -35,16 +35,16 @@ static inline uint64_t axp_logic (int f, uint64_t a, uint64_t b, uint64_t c)
 	const uint64_t amask = 1;			/* bwx		*/
 	const uint64_t impl  = 1;			/* 21164*	*/
 
-	uint64_t bb  = F3 ? ~b : b;			/* 08 - invert	*/
-
-	uint64_t bitop = F6 ? a ^ bb :			/* 40 - xor	*/
-			 F5 ? a | bb : a & bb;		/* 20 - or	*/
-
 	const int lbs = F4;				/* 10 - lbs	*/
 	const int eq  = F5;				/* 20 - eq	*/
 	const int lt  = F6;				/* 40 - lt	*/
 	const int inv = F1 & (F2 | !F4);
 	const int cond = axp_cond (lbs, eq, lt, inv, a);
+
+	uint64_t bb  = F3 ? ~b : b;			/* 08 - invert	*/
+
+	uint64_t bitop = F6 ? a ^ bb :			/* 40 - xor	*/
+			 F5 ? a | bb : a & bb;		/* 20 - or	*/
 
 	int cmov = F2 | F1;				/* 02/04 - cmov	*/
 
